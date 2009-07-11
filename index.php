affc070f040f4ee93de33ae91e47b960c123bd1e
Nat Welch <nat@natwelch.com>
2009-07-11 13:59:42 -0700

    Alright, got basic layout done.

--- index.php
+++ index.php
@@ -3,27 +3,37 @@ require_once 'Flickr/API.php';
 
 # create a new api object
 
-$api =& new Flickr_API(array(
-	'api_key'  => '0970dea5db5023bf4da15b0d7062e1b3',
-	'api_secret' => '...' 
-	));
 
-# call a method
+function display($userid) {
+	$api =& new Flickr_API(array(
+				'api_key'  => '0970dea5db5023bf4da15b0d7062e1b3',
+				'api_secret' => $secret
+				));
 
-$response = $api->callMethod('flickr.test.echo', array(	'foo' => 'bar'	));
+	// call a method
 
+	$response = $api->callMethod('flickr.test.echo', array(	'foo' => 'bar'	));
 
-# check the response
+	// check the response
 
-if ($response){
-	# response is an XML_Tree root object
-var_dump($response);
-}else{
-	# fetch the error
-	$code = $api->getErrorCode();
-	$message = $api->getErrorMessage();
-	var_dump($message);
+	if ($response){
+		// response is an XML_Tree root object
+		var_dump($response);
+	}else{
+		// fetch the error
+		$code = $api->getErrorCode();
+		$message = $api->getErrorMessage();
+		print_r("<pre>\n$code : $message\n </pre>");
+	}
 }
 
 ?>
+
+<html>
+<head>
+<title>GearSpyr</title>
+</head>
+<body>
+</body>
+</html>
 
