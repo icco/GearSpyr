f876f8d95b56db2404b7c195a58fc702dad5c20a
Nat Welch <nat@natwelch.com>
2009-07-11 15:50:21 -0700

    Got a list of photos from a user.

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 require_once 'Flickr/API.php';
 
-# create a new api object
-
+if(isset($_POST['userid'])) 
+	display($_POST['userid']);
 
 function display($userid) {
+	// create a new api object
 	$api =& new Flickr_API(array(
 				'api_key'  => '0970dea5db5023bf4da15b0d7062e1b3',
 				'api_secret' => $secret
@@ -12,19 +13,38 @@ function display($userid) {
 
 	// call a method
 
-	$response = $api->callMethod('flickr.test.echo', array(	'foo' => 'bar'	));
-
-	// check the response
-
-	if ($response){
-		// response is an XML_Tree root object
-		var_dump($response);
-	}else{
+	$response = $api->callMethod('flickr.people.findByUsername', array(
+				'username' => $userid	
+				));
+	if (!$response){
 		// fetch the error
 		$code = $api->getErrorCode();
 		$message = $api->getErrorMessage();
 		print_r("<pre>\n$code : $message\n </pre>");
 	}
+
+	$user = new SimpleXMLElement($response->get());
+	$userNSID = $user->user['nsid'];
+
+	$response = $api->callMethod('flickr.people.getPublicPhotos', array(
+				'user_id' => $userNSID	
+				));
+	if (!$response){
+		// fetch the error
+		$code = $api->getErrorCode();
+		$message = $api->getErrorMessage();
+		print_r("<pre>\n$code : $message\n </pre>");
+	}
+	$userPhotosXML = new SimpleXMLElement($response->get());
+
+	$userPhotos = array();
+
+	foreach($userPhotosXML->photos->photo as $photo) 
+		$userPhotos[] = $photo["id"];
+
+
+
+	print "<pre>"; 	print_r($userPhotos); print "</pre>";
 }
 
 ?>
@@ -34,6 +54,10 @@ function display($userid) {
 <title>GearSpyr</title>
 </head>
 <body>
+<form method="post">
+Username: <input type="text" name="userid" />
+<input type="submit" value="Submit" />
+</form>
 </body>
 </html>
 
